fadd4e0372670372ae376f6a6cee10194365cdc0
Joohyung Park <joohyung.park@medibloc.org>
2024-01-22 06:30:37 +0900

    devbox

--- dapr-distributed-calendar/python/messages.py
+++ dapr-distributed-calendar/python/messages.py
@@ -10,19 +10,22 @@ from dapr.clients import DaprClient
 app = flask.Flask(__name__)
 CORS(app)
 
+
 # dapr calls this endpoint to register the subscriber configuration
 # an alternative way would to be declare this inside a config yaml file
 @app.route('/dapr/subscribe', methods=['GET'])
 def subscribe():
-    subscriptions = [{'pubsubname': 'pubsub',
-                      'topic': 'events-topic',
-                      'route': 'getmsg'}]
+    subscriptions = [
+        {'pubsubname': 'pubsub', 'topic': 'events-topic', 'route': 'getmsg'}
+    ]
     return jsonify(subscriptions)
+
 
 emailTo = 'js26@iitbbs.ac.in'
 subject = 'Testing Dapr binding integration'
 data = ''
 binding_name = 'sendgrid'
+
 
 # subscriber acts as a listener for the topic events-topic
 @app.route('/getmsg', methods=['POST'])
@@ -33,24 +36,18 @@ def subscriber():
     print(data, flush=True)
     send_email()
 
+
 # send_email sends the json payload obtained from pubsub
 # to the dapr output binding for SendGrid
 def send_email():
     with DaprClient() as d:
-            
-        req_data = {
-            'metadata': {
-                'emailTo': emailTo,
-                'subject': subject
-            },
-            'data': data
-        }
+        req_data = {'metadata': {'emailTo': emailTo, 'subject': subject}, 'data': data}
 
-        print(req_data, flush=True)
+        # print(req_data, flush=True)
 
         # Create a typed message with content type and body
-        resp = d.invoke_binding(binding_name, 'create', json.dumps(req_data))
-        print(resp, flush=True)
-            
+        # resp = d.invoke_binding(binding_name, 'create', json.dumps(req_data))
+        print([binding_name, req_data], flush=True)
+
 
 app.run()
